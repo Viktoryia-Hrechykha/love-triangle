6f4060d06ff55407d26adcbe26af2726e1d72708
Viktoryia_Hrechykha <viktoryia_hrechykha@epam.com>
2018-09-20 23:28:20 +0300

    answer

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  
+  let loveTrianglesCount = 0, whoLovedBySp1, whoLovedBySp2, whoLovedBySp3;
+
+  for (let i = 0; i < preferences.length; i++) {
+    whoLovedBySp1 = preferences[i] - 1;
+    whoLovedBySp2 = preferences[whoLovedBySp1] - 1;
+    whoLovedBySp3 = preferences[whoLovedBySp2] - 1;
+
+    if ((whoLovedBySp3 === i) && (whoLovedBySp1 !== whoLovedBySp2) && (whoLovedBySp2 !== whoLovedBySp3) && (whoLovedBySp1 !== whoLovedBySp3)) loveTrianglesCount++;
+  }
+
+  return loveTrianglesCount / 3;
+
 };
